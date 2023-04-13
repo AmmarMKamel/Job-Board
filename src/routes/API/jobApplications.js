c1b07a11d3704816f39596029c30c6b8f7039bde
mohamed <mohamedaymanzolfy@gmail.com>
2023-04-13 12:58:04 -0700

    #

--- src/routes/API/jobApplications.js
+++ src/routes/API/jobApplications.js
@@ -1,1 +1,29 @@
+const express = require("express");
+const jobAppsAPIRouter = express.Router();
+const JobApps = require("../../models/jobApplications");
 
+jobAppsAPIRouter.get("/", (req, res) => {
+  JobApps.findAll()
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err.message));
+});
+
+jobAppsAPIRouter.post("/", (req, res) => {
+  JobApps.create({ ...req.body })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(200).json(err.message));
+});
+
+jobAppsAPIRouter.patch("/", (req, res) => {
+  JobApps.update({ ...req.body }, { where: { id: req.params.id } })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err.message));
+});
+
+jobAppsAPIRouter.delete("/:id", (req, res) => {
+  JobApps.destroy({ where: { id: req.params.id } })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(500).json(err.message));
+});
+
+module.exports = jobAppsAPIRouter;
