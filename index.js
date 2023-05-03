2dc99c18397345f9d703051b8c86ffbc1fc189de
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-05-03 14:32:26 +0300

    Implement sessions and cookies

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const MySQLStore = require("express-mysql-session")(expressSession);
+const mysql = require("mysql");
 
 // Pages routes
 const RegisterRoute = require("./src/routes/register");
@@ -13,6 +17,7 @@ const EmployerRoute = require("./src/routes/employer");
 const SeekerRoute = require("./src/routes/seeker");
 const Job = require("./src/models/Job");
 const JobAppsRoute = require("./src/routes/jobApps");
+const LogoutRoute = require("./src/routes/logout");
 
 // API route
 const APIRouter = require("./src/routes/API/main");
@@ -23,6 +28,14 @@ const { HandleDBModel } = require("./src/helpers/HandleDBModel");
 // Initializing express
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Set up MySQL connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "job_board",
+});
 
 // EJS
 app.set("view engine", "ejs");
@@ -37,6 +50,50 @@ app.use(bodyParser.json());
 app.use(HandleDBModel);
 app.use(multer().array());
 
+// Set up session store
+const sessionStore = new MySQLStore(
+  {
+    expiration: 86400000,
+    createDatabaseTable: true,
+  },
+  db
+);
+
+// Set up session middleware
+app.use(cookieParser());
+app.use(
+  expressSession({
+    secret: "my_secret_key",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 86400000,
+    },
+  })
+);
+
+// Configure the cookie parser middleware
+// app.use(cookieParser());
+
+// Configure the Express session middleware
+// const sessionStore = new expressSession({
+//   secret: "my-secret-key",
+//   resave: false,
+//   saveUninitialized: false,
+//   store: new MySQLStore({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "",
+//     database: "job_board",
+//     table: "sessions",
+//   }),
+// });
+//
+// // Create a new session table
+// app.use(sessionStore);
+
 // Routes
 app.use("/api", APIRouter);
 app.use("/register", RegisterRoute);
@@ -46,6 +103,7 @@ app.use("/jobs", JobListingsRoute);
 app.use("/employer", EmployerRoute);
 app.use("/seeker", SeekerRoute);
 app.use("/jobApps", JobAppsRoute);
+app.use("/logout", LogoutRoute);
 
 // Home route
 app.get("/", async (req, res) => {
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const Seeker = require("../models/Seeker");
-const Employer = require("../models/Employer");
 const LoginRoute = express.Router();
+const Employer = require("../models/Employer");
+const Seeker = require("../models/Seeker");
+
+// Create a middleware function that checks if the user is logged in
+// const isLoggedIn = (req, res, next) => {
+//   const dbModel =
+//     req.url.split("/")[1].charAt(0).toUpperCase() +
+//     req.url.split("/")[1].slice(1);
+//   // Parse the cookies
+//   const cookies = cookieParser(req);
+//
+//   // Get the session id from the cookies
+//   const sessionId = cookies.session;
+//
+//   // Check if there is a session in the database with the same session id
+//   const session = expressMysqlSession.getSessionById(req, sessionId);
+//
+//   // If there is a session, the user is already logged in, so redirect to the /user route
+//   if (session) {
+//     res.redirect(`/${dbModel}/${session.user.id}`);
+//     return;
+//   }
+//   // No session, so log in normally
+//   next();
+// };
 
 async function auth(req, res, next) {
   const dbModel =
@@ -28,16 +51,39 @@ async function auth(req, res, next) {
 }
 
 LoginRoute.get("/", async (req, res) => {
+  if (req.cookies.session_id) {
+    const user = req.session.user;
+    const employer = await Employer.findOne({ where: { email: user.email } });
+    if (employer) {
+      res.status(200).redirect(`/employer/${user.id}`);
+      return;
+    }
+    const seeker = await Seeker.findOne({ where: { email: user.email } });
+    if (seeker) {
+      res.status(200).redirect(`/seeker/${user.id}`);
+      return;
+    }
+  }
   res.status(200).render("login");
 });
 
 // Login as a jobseeker
 LoginRoute.post("/seeker", auth, async (req, res) => {
+  req.session.user = req.user;
+  res.cookie("session_id", req.session.id, {
+    maxAge: 60 * 60 * 24 * 365,
+    httpOnly: true,
+  });
   res.status(200).redirect(`/seeker/${req.user.id}`);
 });
 
 // Login as an employer
 LoginRoute.post("/employer", auth, async (req, res) => {
+  req.session.user = req.user;
+  res.cookie("session_id", req.session.id, {
+    maxAge: 60 * 60 * 24 * 365,
+    httpOnly: true,
+  });
   res.status(200).redirect(`/employer/${req.user.id}`);
 });
 
